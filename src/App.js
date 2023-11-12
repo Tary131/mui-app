50069302cedc9bb8d5f64412907262991f272172
Tary131 <taras.pincak@seznam.cz>
2023-11-12 15:03:13 +0100

    ctreate-shopingListRoute

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ShoppingListDetail from './components/ShoppingListDetail';
-import ShoppingListOverview from './components/ShoppingListOverview';
+import ShoppingListDetail from './routes/ShoppingListDetail';
 
-const AppRouter = () => {
+const App = () => {
 	return (
 		<Router>
 			<Switch>
 				<Route path="/shopping-lists/:id" component={ShoppingListDetail} />
-				<Route path="/shopping-lists" component={ShoppingListOverview} />
+
 			</Switch>
 		</Router>
 	);

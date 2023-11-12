4bf1f5ad0e30ffba6ac7dc758cffda7ed693f48a
Tary131 <taras.pincak@seznam.cz>
2023-11-12 16:18:12 +0100

    ctreate-shopingListRoute

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import "./App.css";
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import ShoppingListDetail from './routes/ShoppingListDetail';
 
 const App = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route path="/shopping-lists/:id" component={ShoppingListDetail} />
-
-			</Switch>
-		</Router>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" component={<ShoppingListDetail />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
